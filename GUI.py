bedc676bdd184fbc10dba35563ec7dc4838f2229
Xin Zhang <mynovmbr@gmail.com>
2018-01-22 00:29:31 -0500

    Add files via upload

--- GUI.py
+++ GUI.py
@@ -1,19 +1,30 @@
 from graphics import *
 import random
 
+point_00 = Point(100, 300)
+point_01 = Point(300, 300)
+point_02 = Point(300, 100)
+point_03 = Point(100, 100)
 
-Point_01 = Point(100, 300)
-Point_02 = Point(300, 300)
-Point_03 = Point(300, 100)
-Point_04 = Point(100, 100)
+class Wall(Point):
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+        self.type = str("Living Room")
+        ##self.length = ((p1.x-p2.x)^2+(p1.y-p2.y)^2)^0.5
 
-wall_A = [Point_01, Point_02] 
-wall_B = [Point_02, Point_03]
-wall_C = [Point_03, Point_04] 
-wall_D = [Point_04, Point_01]
 
-walls = [wall_A, wall_B, wall_C, wall_D]
-Points = [Point_01, Point_02, Point_03, Point_04]
+wall_00 = Wall(point_00, point_01)
+wall_01 = Wall(point_01, point_02)
+wall_02 = Wall(point_02, point_03) 
+wall_03 = Wall(point_03, point_00)
+
+room_type = ["Living Room", "Bedroom", "Kitchen", "Bathroom", "Corridor"]
+room_00 = [wall_00, wall_01, wall_02, wall_03, room_type[1]]
+
+walls = [wall_00, wall_01, wall_02, wall_03]
+points = [point_00, point_01, point_02, point_03]
+rooms = [room_00]
 
 #create graphic window:
 win = GraphWin("My Window", 500, 500)
@@ -37,28 +48,28 @@ def move_a_wall(walls):
 def draw_walls(walls, canvas):
 
 	for wall in walls:
-		wall = Line(wall[0], wall[1])
+		wall = Line(wall.p1, wall.p2)
 		wall.draw(canvas)
 
 
 def move_left(wall):
-	wall[0].x = wall[0].x + 20.0
-	wall[1].x = wall[1].x + 20.0
+	wall.p1.x = wall.p1.x + 20.0
+	wall.p2.x = wall.p2.x + 20.0
 
 
 def move_right(wall):
-	wall[0].x = wall[0].x - 20.0
-	wall[1].x = wall[1].x - 20.0
+	wall.p1.x = wall.p1.x - 20.0
+	wall.p2.x = wall.p2.x - 20.0
 
 
 def move_up(wall):
-	wall[0].y = wall[0].y + 20.0
-	wall[1].y = wall[1].y + 20.0
+	wall.p1.y = wall.p1.y + 20.0
+	wall.p2.y = wall.p2.y + 20.0
 
 
 def move_down(wall):
-	wall[0].y = wall[0].y - 20.0
-	wall[1].y = wall[1].y - 20.0	
+	wall.p1.y = wall.p1.y - 20.0
+	wall.p2.y = wall.p2.y - 20.0	
 
 def clear(canvas):
     for item in canvas.items[:]:
@@ -66,7 +77,7 @@ def clear(canvas):
     win.update()
 
 def main(walls, canvas):
-	for i in range(0, 19):
+	for i in range(0, 9):
 		draw_walls(walls, canvas)
 		move_a_wall(walls)
 		
