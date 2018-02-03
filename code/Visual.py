d0edb842d38ae910b0510a0addc72d486ee1dcf2
Xian Lai <Lai.mcknight@gmail.com>
2018-02-02 21:28:21 -0500

    Update Visual.py

--- code/Visual.py
+++ code/Visual.py
@@ -207,20 +207,3 @@ def main():
 
 if __name__ == "__main__": main()
 
-"""
-fig, ax = plt.subplots()
-line, = ax.plot(np.random.randn(100))
-plt.show(block=False)
-
-tstart = time.time()
-num_plots = 0
-while time.time()-tstart < 5:
-    line.set_ydata(np.random.randn(100))
-    ax.draw_artist(ax.patch)
-    ax.draw_artist(line)
-    fig.canvas.update()
-    fig.canvas.flush_events()
-    num_plots += 1
-print(num_plots/5)
-"""
-
